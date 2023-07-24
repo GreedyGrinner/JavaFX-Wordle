2e6989b3d3aa2fb4c808039852d2a063ddd7c7e6
GreedyGrinner <122286522+GreedyGrinner@users.noreply.github.com>
2023-07-24 13:54:55 +0200

    added files

---
+++ JavaFX_Wordle/src/main/java/app/wordle/LauncherWordle.java
@@ -0,0 +1,9 @@
+package app.wordle;
+
+import javafx.application.Application;
+
+public class LauncherWordle {
+    public static void main(String[] args) {
+        Application.launch(AppWordle.class, args);
+    }
+}
