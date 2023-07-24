6de99448cfdc8129953ffa55d68c737f12639037
GreedyGrinner <122286522+GreedyGrinner@users.noreply.github.com>
2023-07-24 13:49:45 +0200

    Delete JavaFX-Minesweeper/FX_BaseProject directory

--- JavaFX-Minesweeper/FX_BaseProject/src/main/java/app/wordle/LauncherWordle.java
+++
@@ -1,9 +0,0 @@
-package app.wordle;
-
-import javafx.application.Application;
-
-public class LauncherWordle {
-    public static void main(String[] args) {
-        Application.launch(AppWordle.class, args);
-    }
-}
